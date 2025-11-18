96c6002190ff0f7724ade28cf967c2154025b446
Dodik Gaghan <gaghan430@gmail.com>
2025-11-18 13:59:58 +0700

    Update copy and mobile view

--- apps/login/src/app/(main)/(illustration)/register/_login-btn.tsx
+++ apps/login/src/app/(main)/(illustration)/register/_login-btn.tsx
@@ -7,16 +7,21 @@ export const LoginBtn = () => {
   const router = useRouter();
 
   return (
-    <div className="text-center ztdl-p text-sm font-normal flex items-center gap-0.5 mt-6">
-      <Translated i18nKey="alreadyRegistered" namespace="register" />
-      <button
-        className="underline"
-        onClick={() => router.replace("/loginname")}
-        type="button"
-        data-testid="register-button"
-      >
-        <Translated i18nKey="loginNow" namespace="register" />
-      </button>
-    </div>
+    <>
+      <div className="text-center ztdl-p text-sm font-normal flex items-center gap-0.5 mt-6">
+        <Translated i18nKey="alreadyRegistered" namespace="register" />
+        <button
+          className="underline"
+          onClick={() => router.replace("/loginname")}
+          type="button"
+          data-testid="register-button"
+        >
+          <Translated i18nKey="loginNow" namespace="register" />
+        </button>
+      </div>
+      <div className="text-center ztdl-p text-sm font-normal flex items-center gap-0.5 mt-6">
+        <Translated i18nKey="onWaitList" namespace="register" />
+      </div>
+    </>
   );
 };
